a9c93c723a5199a6ffa9dd6ae62d254a9820b830
Tehila <94409393+Tehilalev@users.noreply.github.com>
2022-07-04 17:05:04 +0300

    Update Divided_Differences_Table.py

--- Divided_Differences_Table.py
+++ Divided_Differences_Table.py
@@ -1,21 +1,18 @@
 def divided_differences_table(table):
     s = 1
-    dividing = []
-    all_solution = []
+    dividing = []  # for each dividing
+    all_solution = []  # for all dividing
     for j in range(0, len(table) - 1):
         solution = []
-        if j == 0:
+        if j == 0:  # the first dividing
             for i in range(0, len(table) - j - 1):
                 solution.append((table[i + 1][1] - table[i][1]) / (table[i + s][0] - table[i][0]))
-        else:
+        else:  # for other dividing
             for i in range(0, len(table) - j - 1):
                 solution.append((dividing[i + 1] - dividing[i]) / (table[i + s][0] - table[i][0]))
         dividing = solution
         s += 1
-        print(solution)
         all_solution.append(solution)
-
-    print(all_solution)
 
     print("x\t  f(x)\n-----------------")  # print the title
     for i in range(0, len(table)):
